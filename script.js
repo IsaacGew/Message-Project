72421060ddab3f54ea98344c442eb94ae3783333
IsaacGew <75827521+IsaacGew@users.noreply.github.com>
2021-01-17 15:18:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const sendBtn = document.querySelector('#sendBtn')
+const messageIn = document.querySelector('#messageIn')
+const messageOut = document.querySelector('#messageOut')
+
+
+sendBtn.addEventListener('click', sendMsg)
+
+
+function sendMsg (){
+    let content = messageIn.value;
+    if(content === ''){
+        alert('Please Enter Valid Value. Current Value Is Empty')
+    } 
+    else{
+        messageOut.innerHTML = content;
+        messageIn.value = '';
+    }
+    
+}
+
+
+
+
+
+
